1ba3c07c00e92a9fee3530e5a3f708b0c64f275a
root <root@debian.micasa.lan>
2023-01-21 10:46:14 -0300

    Reestructuracion del documento
    Reestructuramos el documento HTML definiendo 3 sectores principales
    	- Cabecera: donde colocamos la barra de navegacion y la presentacion
    	- Cuerpo: donde colocamos un area de texto que contendra informacion de cada elemento de la barra de navegacion
    	  y una columna que servira de guia cuando el texto sea muy largo
    	- Pie de pagina: donde contendra datos del desarrollo y contacto con el desarrollador

--- js/comportamiento.js
+++ js/comportamiento.js
@@ -1,19 +1,14 @@
-import info from './recursos/info.json' assert {tyoe:'JSON'};
-console.log(info);
-
-
-
-
-
 document.getElementById("inicio").addEventListener('click',pagina_inicio());
-document.getElementById("item1").addEventListener('click',modifica("item1",));
+document.getElementById("datospersonales").addEventListener('click',modifica());
 
-
-
+pagina_inicio();
 
 
 function pagina_inicio(){
-	document.getElementById('cargo').innerHTML="Bienvenido/a a 'Mi CV Onnline' ";
-	document.getElementById('tiempo-cargo').innerHTML="Desarrollado por Franco Cristian-Argentina Programa";
-	document.getElementById('descripcion-cargo').innerHTML="Bienvenido/a a 'Mi CV Onnline' ";
+	document.getElementById('cuerpo').innerHTML="<h1 id='bienvenido'>Bienvenido/a a 'Mi CV Onnline'</h1>\n<h2 >Proyecto final en FrontEnd</h2>\n<p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce eu rutrum diam, nec consectetur ante. Etiam interdum justo et ante venenatis, eu venenatis justo suscipit. Nulla rhoncus sit amet turpis in malesuada. Proin ultrices aliquet sem, in tristique lectus. Ut ac magna tortor. Phasellus dapibus odio non venenatis consequat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras sed nisl sed sapien posuere porta. Nullam ultricies eros at ultrices placerat. Donec sollicitudin, est ac sollicitudin tempus, turpis felis tristique orci, eget consectetur dolor sem feugiat mi. Vivamus sem ante, interdum quis fringilla at, rutrum scelerisque sem. Curabitur tempus lectus vitae mauris luctus lobortis. Suspendisse potenti. Donec diam arcu, tristique sed erat gravida, suscipit ultrices dui. Maecenas vehicula magna non sodales finibus. Etiam porta nulla in urna feugiat scelerisque. Ut id sollicitudin ligula. Quisque in nunc in enim scelerisque ultrices. Cras nec auctor nulla. Nunc efficitur bibendum orci tincidunt faucibus. Ut imperdiet feugiat ante non posuere. Nunc tellus dui, vestibulum quis lorem vel, vulputate dapibus eros. Curabitur molestie sapien in nibh egestas, at venenatis nisl hendrerit. Nullam pharetra sollicitudin faucibus. Integer mollis maximus est eu cursus. Etiam libero lectus, euismod eu erat in, sodales tincidunt ligula. Morbi ullamcorper massa ac mi cursus malesuada. Vestibulum feugiat fermentum magna, nec facilisis mi. Vestibulum blandit auctor finibus.</p>\n<h1 id='autor'>Desarrollado por Franco Cristian</h1>\n<p >Año 2023, Febrero 1</p>\n"
+}
+
+function modifica(){
+	const cuerpo = document.getElementById('cuerpo');
+	console.log(cuerpo);
 }
